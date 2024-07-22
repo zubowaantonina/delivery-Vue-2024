d383b672f5c579abffccc228398955a34f8bb3dd
zubowaantonina <zubowaantonina@yandex.ru>
2024-07-22 18:47:46 +0300

    add base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'delivery-Vue-2024',
+  base:'/delivery-Vue-2024',
   plugins: [
     vue(),
   ],
